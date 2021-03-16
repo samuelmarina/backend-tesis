1335148127a4b70dbb2dd673c49f9cc681ab2c69
samuelmarina <samuellmarina@gmail.com>
2021-03-15 21:57:27 -0400

    Crear view para login

--- core/views.py
+++ core/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import jwt
 
-# Create your views here.
+from firebase_admin import credentials, db, initialize_app
+
+
+cred = credentials.Certificate('./firebase-sdk.json')
+initialize_app(cred, {
+    'databaseURL': 'https://tesis-406cd-default-rtdb.firebaseio.com/',
+})
+
+
+class Login(APIView):
+    def post(self, request, *args, **kwargs):
+        """ Request to log in an user or create a new one
+        Returns
+        -------
+        list
+            a list of all the projects of the user
+        """
+        token = request.data['token']
+        user = jwt.decode(token, 'secret', algorithms=['HS256'])
+        user_id = str(user['userid'])
+        user_ref = db.reference('/users/' + user_id)
+        user_ref.update({
+            'name': user['name']
+        })
+        projects_ref = db.reference('/users/' + user_id + '/projects')
+        return Response(projects_ref.get())
