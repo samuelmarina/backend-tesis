97a677702546dce0fb62fd2308f604f2a3bb702c
samuelmarina <samuellmarina@gmail.com>
2021-03-21 15:16:46 -0400

    Crear función para eliminar una arquitectura

--- core/helpers/arquitecturas/arquitecturas.py
+++ core/helpers/arquitecturas/arquitecturas.py
@@ -72,3 +72,51 @@ def addNewArchitecture(elems, architecture, project_index, uid):
         'architectures': architectures
     })
     return architectures
+
+
+def handleDeleteArchitecture(data):
+    """ Manejar la eliminación de una arquitectura de
+    un proyecto de la base de datos del usuario
+
+    Parameters
+    ----------
+    data: dict
+        diccionario con la información de la solicitud
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las arquitecturas del usuario
+    """
+    uid = data['user_id']
+    project_index = data['project_index']
+    arch_index = int(data['arch_index'])
+    url = '/users/' + uid + '/projects/' + str(project_index)
+    architectures = deleteArchitecture(url, arch_index)
+    return architectures
+
+
+def deleteArchitecture(url, archIndex):
+    """ Eliminar una arquitectura de la base de datos
+    del usuario
+
+    Parameters
+    ----------
+    url: str
+        dirección de la base de datos
+    archIndex: int
+        índice de la arquitectura
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las arquitecturas del usuario
+    """
+    arch_ref = db.reference(url + '/architectures')
+    arch_arr = arch_ref.get()
+    arch_arr.pop(archIndex)
+    project_ref = db.reference(url)
+    project_ref.update({
+        'architectures': arch_arr
+    })
+    return arch_arr
