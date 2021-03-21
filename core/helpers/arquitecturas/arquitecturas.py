8751e26e3ab47c66ee65dd66161caa42f73ebc41
samuelmarina <samuellmarina@gmail.com>
2021-03-21 15:25:57 -0400

    Agregar función para editar una arquitecturas

--- core/helpers/arquitecturas/arquitecturas.py
+++ core/helpers/arquitecturas/arquitecturas.py
@@ -120,3 +120,54 @@ def deleteArchitecture(url, archIndex):
         'architectures': arch_arr
     })
     return arch_arr
+
+
+def handleEditArchitecture(data):
+    """ Manejar la edición del nombre de una arquitectura
+    de la base de datos del usuario
+
+    Parameters
+    ----------
+    data: dict
+        diccionario con la información de la solicitud
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las arquitecturas del usuario
+    """
+    uid = data['user_id']
+    project_index = data['project_index']
+    arch_index = int(data['arch_index'])
+    new_arch_name = data['arch_name']
+    url = '/users/' + uid + '/projects/' + str(project_index)
+    architectures = editArchitecture(url, arch_index, new_arch_name)
+    return architectures
+
+
+def editArchitecture(url, archIndex, archName):
+    """ Editar el nombre de una arquitecturas de la 
+    base de datos del usuario 
+
+    Parameters
+    ----------
+    url: str
+        dirección de la base de datos
+    archIndex: int
+        índice de la arquitectura
+    archName: str
+        nuevo nombre de la arquitectura
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las arquitecturas del usuario
+    """
+    arch_ref = db.reference(url + '/architectures')
+    arch_arr = arch_ref.get()
+    arch_arr[archIndex]['name'] = archName
+    project_ref = db.reference(url)
+    project_ref.update({
+        'architectures': arch_arr
+    })
+    return arch_arr
