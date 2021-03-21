c33a5f3443c4cc68b4d90629883a0b93b5fa52c7
samuelmarina <samuellmarina@gmail.com>
2021-03-21 15:33:25 -0400

    Crear función para eliminar versiones

--- core/helpers/versiones/versiones.py
+++ core/helpers/versiones/versiones.py
@@ -33,3 +33,55 @@ def createNewVersion(data):
         'versions': versions_array
     })
     return versions_array
+
+
+def handleDeleteVersion(data):
+    """ Manejar la eliminación de una versión de la
+    base de datos del usuario
+
+    Parameters
+    ----------
+    data: dict
+        diccionario con la información de la solicitud
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las versiones de una 
+        arquitectura del usuario
+    """
+    uid = data['user_id']
+    ver_index = int(data['ver_index'])
+    arc_index = str(data['arc_index'])
+    project_index = str(data['project_index'])
+    url = '/users/' + uid + '/projects/' + \
+        project_index + '/architectures/' + arc_index
+    versions = deleteVersion(url, ver_index)
+    return versions
+
+
+def deleteVersion(url, verIndex):
+    """ Eliminar una versión de la base de datos
+    del usuario
+
+    Parameters
+    ----------
+    url: str
+        dirección de la base de datos
+    verIndex: int
+        índice de la versión
+
+    Returns
+    -------
+    list
+        lista actualizada con todas las versiones de una 
+        arquitectura del usuario
+    """
+    version_ref = db.reference(url + '/versions/')
+    versions_arr = version_ref.get()
+    versions_arr.pop(verIndex)
+    arc_ref = db.reference(url)
+    arc_ref.update({
+        'versions': versions_arr
+    })
+    return versions_arr
